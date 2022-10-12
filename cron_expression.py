d81c4af7a53cdf79cce09b4af48683ede75f0040
odulaja philip <odulajaphilip@gmail.com>
2022-10-12 11:56:46 +0100

    Added minute validator

--- cron_expression.py
+++ cron_expression.py
@@ -9,5 +9,10 @@ def cron_expressor(cron):
     """
         expression validity check
     """
-    # checks for whitespace in cron expression
-    if len(cron.split()) != 5: return "Invalid cron expression"   
+    # checks for whitespace in cron expression and invalid minute entry
+    if len(cron.split()) != 5 or int(cron.split()[0]) > 60 : return "Invalid cron expression"  
+
+    
+
+
+print(cron_expressor('* * * * *'))
