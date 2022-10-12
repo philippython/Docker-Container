1faff5aa0452174b150ddfadd0c23fd60eebf51d
odulaja philip <odulajaphilip@gmail.com>
2022-10-12 11:49:18 +0100

    Validity chek one

--- cron_expression.py
+++ cron_expression.py
@@ -6,3 +6,8 @@ from datetime import datetime
 
 def cron_expressor(cron):
 
+    """
+        expression validity check
+    """
+    # checks for whitespace in cron expression
+    if len(cron.split()) != 5: return "Invalid cron expression"   
