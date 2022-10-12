56604827a076e85bc59b89f72df9b7a66ad9fb69
odulaja philip <odulajaphilip@gmail.com>
2022-10-12 12:26:47 +0100

    More logic

--- cron_expression.py
+++ cron_expression.py
@@ -4,6 +4,24 @@ The function below returns a human readable date and time format after receiving
 """
 from datetime import datetime
 
+
+def min_hr_mon_validator(index, cron):
+    try :
+        int(cron.split()[index])
+    except ValueError:
+        pass
+    else:
+        if index == 0 :
+            if int(cron.split()[index]) > 59 : return "Invalid minute"
+        elif index == 1 :
+            if int(cron.split()[index]) > 23 : return "Invalid hour"
+        elif index == 2 :
+            if int(cron.split()[index]) > 31 and int(cron.split()[3]) != 4 : return "Invalid minute"
+
+
+
+
+
 def cron_expressor(cron):
 
     """
