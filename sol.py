852fb0e246119904d841ea13a2020b775d8cdfbc
odulaja philip <odulajaphilip@gmail.com>
2022-10-12 18:49:55 +0100

    created sol.py

---
+++ sol.py
@@ -0,0 +1,26 @@
+# Given the following string: "race bear thing care keen trap tweet earth knee heart neek tewt"
+# Write a function which takes as input this string and returns as
+#  a result a dictionary of items, which contains the words from the given string as 
+#  keys and as values a list with its anagrams from within the input string
+#   if there are any (if there are not, an empty list shall be used as value for the specific keyword). 
+#  Anagrams already found for a specific word shall not be considered new keys in the dictionary.
+
+
+string = "race bear thing care keen trap tweet earth knee heart neek tewt"
+
+def get_anagrams(string):
+    words_list = string.split()
+    output = {}
+    for word in words_list:
+        for figure in words_list:
+                if sorted(word) == sorted(figure) and len(word) == len(figure) and word != figure:
+                        output[word] = [figure]
+        if word not in output.keys():
+            output[word] = []
+        if [word] in list(output.values()):
+            del output[word]  
+    return output
+
+
+    
+print(get_anagrams(string))
