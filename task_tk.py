e8a8079b77da7d9e5a13eefbf1993d3b1970eecf
odulaja philip <odulajaphilip@gmail.com>
2022-10-15 18:19:31 +0100

    used grid for positioning

--- task_tk.py
+++ task_tk.py
@@ -1,14 +1,14 @@
-from tkinter import *
 import tkinter
 
 # creating Tk window object
-window = Tk()
+window = tkinter.Tk()
 window.title("Calculator")
-window.maxsize(width=200, height=200)
+window.minsize(width=200, height=200)
 
 # total sum label 
-total = tkinter.Label(text="Total", font=("Arial", 12 , "bold"))
-total.pack(side="left")
+# using the grid function for goementry
+total = tkinter.Label(text="Total: ", font=("Arial", 12 , "bold"))
+total.grid(column=1, row=1)
 
 
 # the code below keeps the window open
