951116717456c49619ba55021cf1e6a130da9d68
odulaja philip <odulajaphilip@gmail.com>
2022-10-17 16:27:35 +0100

    Aggregation completed

--- task_oop.py
+++ task_oop.py
@@ -1,28 +1,73 @@
-# songs, artists and playlist
+# creating the Artist Class
+class Artist:
 
-#  creating the song class
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.songs = [] 
+
+    #  This method adds new song to the Artist songs list
+    def make_song(self, song):
+        self.songs.append(song)
+      
+    # This method display all songs of this artist
+    def all_songs(self):
+        print("<<<<<<<<<<<<<<<<<<<<<<  All Songs by %s >>>>>>>>>>>>>>>>>>>>>>>>>" % (self.name))
+        for song in self.songs:
+            print(song)
+        print("-----------------------------------------------------")
+
+#  creating the Song class
 class Song:
 
-    def __init__(self, title: str, artist: str, duration: int, category: str) -> None:
+    def __init__(self, title: str, artist: object, duration: float) -> None:
         self.title = title
-        self.artist = artist
+        self.artist_name = artist.name
         self.duration = duration
 
-        # song category can vary from HipHop, gospel, Rap, drill and so much more
-        self.category = category
+        # The code below links a song to an artist
+        artist.make_song(self)
+        
 
-#  creating the song class
+    # this method is a string representation of our song object 
+    def __str__(self) -> str:
+        return "%s by %s duration: %smins" % (self.title, self.artist_name, self.duration)
+
+#  creating the Playlist class
 class Playlist:
 
-
-    def __init__(self) -> None:
+    def __init__(self, name) -> None:
+        self.name = name
         self.playlist = []
 
+    #  This method adds new song to the playlist
     def add_song(self, song):
         self.playlist.append(song)
 
-    def display(self):
-        
+    def display(self):      
+        print("<<<<<<<<<<<<<< Playlist (%s) >>>>>>>>>>>" % (self.name))  
         for song in self.playlist:
             print(song)
+        print("-----------------------------------------------------")
 
+
+#  Testing aggregation 
+
+# creating an Artist object
+asake = Artist("Ololade Asake", 23)
+nle_choppa = Artist("NLE Choppa", 21)
+
+# creating songs object
+sungba = Song("Sungba", asake, 3.20)
+dull = Song("Dull", asake, 2.10)
+Me_vs_me = Song("Me Vs Me", nle_choppa, 5.00)
+
+# creating Playlist object
+hiphop_playlist = Playlist("HipHop Playlist")
+hiphop_playlist.add_song(sungba)
+hiphop_playlist.add_song(dull)
+hiphop_playlist.add_song(Me_vs_me)
+
+asake.all_songs()
+nle_choppa.all_songs()
+hiphop_playlist.display()
