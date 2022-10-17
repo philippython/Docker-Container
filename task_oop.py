604ce687c1a0b1667553bcd570460b8895361571
odulaja philip <odulajaphilip@gmail.com>
2022-10-17 15:32:58 +0100

    Adding comments

--- task_oop.py
+++ task_oop.py
@@ -11,11 +11,18 @@ class Song:
         # song category can vary from HipHop, gospel, Rap, drill and so much more
         self.category = category
 
-
+#  creating the song class
 class Playlist:
 
 
     def __init__(self) -> None:
         self.playlist = []
 
-    def display
+    def add_song(self, song):
+        self.playlist.append(song)
+
+    def display(self):
+        
+        for song in self.playlist:
+            print(song)
+
