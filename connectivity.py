409969c3656ef5fefcc436ad577f14088b308c19
odulaja philip <odulajaphilip@gmail.com>
2022-10-19 17:33:41 +0100

    error handling

--- connectivity.py
+++ connectivity.py
@@ -7,13 +7,24 @@ I would be sending API requests to the following websites
 
 Using the requests module
 """
-import requests 
+from requests import *
 
 website_list = ["https://github.com/", "https://stackoverflow.com/", "https://amazon.com/"]
-# making requests to all websites using Mapping
+header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" ,
+    'referer':'https://www.google.com/'
+}
 
-#  the code below sends a requests to all websites in the website_list
-connectivity_mapper = map(lambda url : requests.get(url).status_code, website_list)
+def requester(url):
 
-#  getting all status_code of request made
-status_codes = list(connectivity_mapper)
+    try:
+        response = get(url, headers=header)
+    except exceptions.ConnectionError:
+        return 404
+    else:
+        return response.status_code
+
+success_response = [requester(url) if requester(url) == 200 else None for url in website_list]
+error_response =  [requester(url) if requester(url) != 200 else None for url in website_list]
+print(success_response)
+print(error_response)
