9bd3fd15934f51530bab2a40d2033dc062fbdd9e
odulaja philip <odulajaphilip@gmail.com>
2022-10-19 12:53:37 +0100

    Handle process not found error

--- process_monitor.py
+++ process_monitor.py
@@ -26,7 +26,7 @@ def processor():
     # this code below compares current running process and the running process 20seconds earlier
     if len(processor_list) == 1: 
         print('CPU usage (GB) in %:', cpu_usage[0])
-        print("We have %s processes runnning" % (processor_list[0]))
+        print("We have %s processes running................." % (processor_list[0]))
     
     if len(processor_list) > 1:
         difference_in_processes = abs(processor_list[-1] - processor_list[-2])
@@ -35,19 +35,29 @@ def processor():
         
         #  checks if the change in ram usage is above 10% the previous
         if diff_in_cpu_usage > prev_ten_percent_usage:
-            pass
+            for process in wmi_object.Win32_Process():
+
+                try:
+                    p = psutil.Process(process.ProcessId)
+                except psutil.NoSuchProcess:
+                    pass
+                else:
+                    each_process_usage = p.cpu_percent()
+                    if each_process_usage > 1 :
+                        print(f"Process {process} is using more than 1 % of CPU ")
+                
 
         if processor_list[-1] > processor_list[-2] :
             print('CPU usage (GB) in %:', cpu_usage[-1])
-            print("We have %s more processes than 20 seconds earlier" % (difference_in_processes))
+            print("We have %s more processes than 20 seconds earlier........." % (difference_in_processes))
         
         elif processor_list[-1] == processor_list[-2] :
             print('CPU usage (GB) in %:', cpu_usage[-1])
-            print("We have no new processes than 20 seconds earlier")
+            print("We have no new processes than 20 seconds earlier............")
 
         else:
             print('CPU usage (GB) in %:', cpu_usage[-1])
-            print("We have %s less processes than 20 seconds earlier" % (difference_in_processes))
+            print("We have %s less processes than 20 seconds earlier..........." % (difference_in_processes))
 
     time.sleep(20)
 
