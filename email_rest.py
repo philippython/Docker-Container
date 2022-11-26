1d81238e41aa44eb178ceff2baec4dee5bd42f24
odulaja philip <odulajaphilip@gmail.com>
2022-11-26 17:06:47 +0100

    refactored code for ssl mail sending

--- email_rest.py
+++ email_rest.py
@@ -1,7 +1,11 @@
-from smtplib import SMTP
+from smtplib import SMTP_SSL
+from email.message import EmailMessage
+# from dotenv import load_env
+import ssl
 import requests
 import os
 
+# load_env()
 
 API_ENDPOINT = 'https://user-mangement-api.herokuapp.com/'
 SMTP_HOST = "smtp.gmail.com"
@@ -11,22 +15,25 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75\
             Safari/537.36'
            }
+
 # MAIL_SENDER = os.getenv("SENDER_EMAIL")
 # MAIL_SENDER_PASSWORD = os.getenv("GOOGLE_APP_PASSWORD")
 print("running")
 
-# setting up mail configuration
+# Function below sends mail and request update using ssl and smtp
 def send_update(status, json):
-    with SMTP(SMTP_HOST, port=465) as connection:
+    mail = EmailMessage()
+    mail.to, mail.subject , mail['from'] = MAIL_SENDER, f"Subject:Post Request Update:{status}" , MAIL_SENDER
+    mail.set_content(json)
+
+    context = ssl.create_default_context()
+    with SMTP_SSL(SMTP_HOST, port=465, context=context) as connection:
         connection.starttls()
         connection.login(user=MAIL_SENDER, password=MAIL_SENDER_PASSWORD)
 
-        # previous code no parameter in sendmail called subject
-        # connection.sendmail(from_addr=MAIL_SENDER, to_addrs=MAIL_SENDER, subject=f"Post Request Update:{status}", msg=json)
-
         connection.sendmail(from_addr=MAIL_SENDER,
                             to_addrs=MAIL_SENDER, 
-                            msg=f"Subject:Post Request Update:{status}\n\n{json}")
+                            msg=mail.as_string())
 
 data = {
         "firstname": "tunji",
@@ -38,7 +45,9 @@ data = {
 }
 
 add_user_request = requests.post(url=f'{API_ENDPOINT}/add_user', data=data)
+print(add_user_request.json())
 if add_user_request.status_code == 200:
+    print(add_user_request.json())
     send_update("Successful", add_user_request.json())
 else:
     send_update("Failed", add_user_request.json())
