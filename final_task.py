d36216ab8c11b4cbf6c734f6b90091f654928a78
odulaja philip <odulajaphilip@gmail.com>
2022-12-13 23:53:44 +0100

    iniitited final task solution

---
+++ final_task.py
@@ -0,0 +1,6 @@
+Job_name = input("What Job are you in search of ? Enter a Job title e.g Python developer")
+
+# API_ENDPOINTs for getting job openings
+ARBIETNOW_API_ENDPOINT = "https://arbeitnow.com/api/job-board-api"
+REED_UK_ENPOINT = "https://www.reed.co.uk/developers"
+THE_MUSE_ENDPOINT = "https://www.themuse.com/developers/api/v2"
